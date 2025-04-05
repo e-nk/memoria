8c454b4640d07df70dad84df07d7bdcf83bf6d22
e-nk <enocklagat747@gmail.com>
2025-04-05 11:56:56 +0300

    updated the home layout to include the AuthProvider

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,13 +1,16 @@
 import { ReactNode } from 'react';
+import { AuthProvider } from '@/context/AuthContext';
 import AuthLayout from '@/components/layouts/AuthLayout';
 import { Providers } from '../providers';
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
     <Providers>
-      <AuthLayout>
-        {children}
-      </AuthLayout>
+      <AuthProvider>
+        <AuthLayout>
+          {children}
+        </AuthLayout>
+      </AuthProvider>
     </Providers>
   );
 }
